e136c380c79eb993c8d988557aec0b7b6bc05589
Felipe Lopes Avila <f.avila@hotmail.com.br>
2022-08-21 18:11:50 -0300

    Criado Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Container } from './styles';
+import { Container, Header, LogoImg, Title, MenuContainer, MenuItemLink } from './styles';
+
+import {MdDashboard, MdArrowDownward, MdArrowUpward, MdExitToApp} from 'react-icons/md';
+
+import Logo from '../../assets/logo.svg'
 
 const Aside: React.FC = () => {
   return  (
     <Container>
-      <h1>Aside</h1>
+      <Header>
+        <LogoImg src={Logo} alt="Logo Minha Carteira" />
+        <Title>Minha carteira</Title>
+      </Header>
+
+      <MenuContainer>
+        <MenuItemLink href="#">
+          <MdDashboard />
+          Dashboard
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowUpward />
+          Entradas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdArrowDownward />
+          Saídas
+        </MenuItemLink>
+
+        <MenuItemLink href="#">
+          <MdExitToApp />
+          Sair
+        </MenuItemLink>
+      </MenuContainer>
     </Container>
   );
 }
--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,7 +3,47 @@ import styled from 'styled-components';
 export const Container = styled.div`
   grid-area: aside;
 
-  color: ${props => props.theme.colors.white};
   background-color: ${props => props.theme.colors.secondary};
-  
+  padding-left: 20px;
+  border-right: 1px solid ${props => props.theme.colors.gray}
+`;
+
+export const Header = styled.div`
+  display: flex;
+  height: 70px;
+  align-items: center;
+`;
+export const LogoImg = styled.img`
+  height: 40px;
+  width: 40px;
+`;
+
+export const Title = styled.h3`
+  color: ${props => props.theme.colors.white};
+  margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+  display: flex;
+  align-items: center;
+
+  color: ${props => props.theme.colors.info};
+  text-decoration: none;
+
+  margin: 10px 0;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  svg {
+    font-size: 18px;
+    margin-right: 5px;
+  }
 `;
